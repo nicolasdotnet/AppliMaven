8194458ed0e5aaae31066351eba73e49ac14ebfe
nicolasdotnet <nicolas.desdevises@gmail.com>
2019-03-01 10:54:49 +0100

    modification fichiers Test de Netbeans Junit 5 + ajout dependancies jackson pour json

---
+++ src/test/java/com/mycompany/mavenproject1/AppMainTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author pi
+ */
+public class AppMainTest {
+    
+    public AppMainTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    
+    /**
+     * Test of division method, of class AppMain.
+     */
+    @Test
+    public void testDivision() {
+        System.out.println("division");
+        double a = 0.0;
+        double b = 0.0;
+        double expResult = 0.0;
+        double result = AppMain.division(a, b);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
